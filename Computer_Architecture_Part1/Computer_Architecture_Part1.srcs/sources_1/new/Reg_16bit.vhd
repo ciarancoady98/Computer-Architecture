4c301afd3bcb5c1d88450d2ef4de058f25b09093
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-02-07 12:07:39 +0000

    Intial Project Upload
    Initial project, includes the sample multiplexer and test bench, along with a 16bit register

---
+++ Computer_Architecture_Part1/Computer_Architecture_Part1.srcs/sources_1/new/Reg_16bit.vhd
@@ -0,0 +1,52 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07.02.2019 11:49:36
+-- Design Name: 
+-- Module Name: Reg_16bit - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Reg_16bit is
+    Port ( D : in STD_LOGIC_VECTOR (15 downto 0);
+           Q : out STD_LOGIC_VECTOR (15 downto 0);
+           Load : in STD_LOGIC;
+           Clk : in STD_LOGIC);
+end Reg_16bit;
+
+architecture Behavioral of Reg_16bit is
+
+begin
+process(Clk)
+    begin
+    if(rising_edge(Clk)) then
+        if load='1' then
+        Q<=D after 5 ns;
+        end if;
+    end if;
+end process;
+end Behavioral;
